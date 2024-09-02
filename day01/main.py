1756f7094ffbea2b008e1a89a70431c16b42d29a
byte-100 <larch_cent_0t@icloud.com>
2024-09-01 22:29:16 -0400

    Create main.py
    create main.py

---
+++ day01/main.py
@@ -0,0 +1,4 @@
+print("hello this is a practice to making a band name generator \n")
+town = input("what city were you born in ?\n")
+pet = input("what is your pet name ?\n")
+print("your name band name is "+ town + pet + "!!")
