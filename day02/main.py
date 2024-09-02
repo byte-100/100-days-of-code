3010e8967f726d88116d3d025350b4af0edafa35
byte-100 <larch_cent_0t@icloud.com>
2024-09-01 23:32:17 -0400

    leaving notes to help with project

--- day02/main.py
+++ day02/main.py
@@ -1,1 +1,5 @@
-'hello test from terminal'
+#'hello test from terminal'
+#world
+#this was done firstly by using the echo to create
+#then second to append using the echo "stuff here" >>main.py
+#lastly we opened nano to add more text using the inbuilt text editor. 
