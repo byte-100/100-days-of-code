3d061cb8e6528b1610460fa3f179fff231f570a2
byte-100 <larch_cent_0t@icloud.com>
2024-09-02 21:28:14 -0400

    update to main.py

--- day02/main.py
+++ day02/main.py
@@ -3,3 +3,16 @@
 #this was done firstly by using the echo to create
 #then second to append using the echo "stuff here" >>main.py
 #lastly we opened nano to add more text using the inbuilt text editor. 
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+
+amount =(1 + tip/100) * bill
+print(amount)
+
+total_pp = amount/people
+total_pp = round(total_pp,2)
+
+print(f"the total per person is ${total_pp}")
